f4a3a69cc63297726dfc9b95efcdca0463f437e2
selplacei <35859522+selplacei@users.noreply.github.com>
2020-07-11 00:09:08 -0700

    Update gen.py

--- gen.py
+++ gen.py
@@ -1,11 +1,13 @@
 # Generates random numbers of specified lengths
 import random
+import string
 import sys
 
 while c := sys.stdin.readline():
+    c = c.strip()
     if not c.isdigit():
         continue
     for _ in range(int(c)):
-        sys.stdout.write(str(random.randint(0, 10)))
+        sys.stdout.write(str(random.choice(string.digits)))
     sys.stdout.write('\n')
     sys.stdout.flush()
